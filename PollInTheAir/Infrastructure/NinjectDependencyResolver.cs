d78250b93048d90733003d5c4df5bb63a33a3c85
thiagosenador <thiagosenadors@gmail.com>
2016-01-17 00:01:04 -0600

    Ninject dependency resolver

---
+++ PollInTheAir/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using PollInTheAir.Domain.Abstract;
+using PollInTheAir.Domain.Concrete;
+
+namespace PollInTheAir.Web.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernel)
+        {
+            this.kernel = kernel;
+
+            AddBindings();
+        }
+
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+
+        private void AddBindings()
+        {
+            kernel.Bind<IPollRepository>().To<PollRepository>();
+            kernel.Bind<IQuestionRepository>().To<QuestionRepository>();
+        }
+    }
+}
