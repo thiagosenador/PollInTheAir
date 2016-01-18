48601b6838702284c241140b615c6c199bb3b5ca
thiagosenador <thiagosenadors@gmail.com>
2016-01-17 20:27:49 -0600

    adding free text question

--- PollInTheAir/Controllers/HomeController.cs
+++ PollInTheAir/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
-﻿using System.Web.Mvc;
-using PollInTheAir.Domain.Concrete;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 using PollInTheAir.Domain.Models;
 
 namespace PollInTheAir.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private List<Question> questions = new List<Question>(); 
+
         public ViewResult Index()
         {
             return View();
@@ -25,13 +27,30 @@ namespace PollInTheAir.Web.Controllers
         [HttpPost]
         public ViewResult CreateMultipleChoicesQuestion(MultipleChoicesQuestion question)
         {
-            using (var context = new PollDbContext())
-            {
-                context.Questions.Add(question);
-                context.SaveChanges();
-            }
+            this.questions.Add(question);
 
             return View();
+        }
+
+        [HttpGet]
+        public ViewResult CreateFreeTextQuestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CreateFreeTextQuestion(Question question)
+        {
+            this.questions.Add(question);
+
+            return View();
+        }
+
+        public ViewResult FinishQuestion()
+        {
+            this.ToString();
+
+            return null;
         }
     }
 }
