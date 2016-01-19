1e132666d51aa4f74893b6752a6e694e30928a52
thiagosenador <thiagosenadors@gmail.com>
2016-01-18 22:24:52 -0600

    fixing data bindings

--- PollInTheAir/Controllers/HomeController.cs
+++ PollInTheAir/Controllers/HomeController.cs
@@ -1,56 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Web.Mvc;
-using PollInTheAir.Domain.Models;
+﻿using System.Web.Mvc;
 
 namespace PollInTheAir.Web.Controllers
 {
     public class HomeController : Controller
     {
-        private List<Question> questions = new List<Question>(); 
-
         public ViewResult Index()
         {
             return View();
-        }
-
-        public ViewResult CreatePoll()
-        {
-            return View();
-        }
-
-        [HttpGet]
-        public ViewResult CreateMultipleChoicesQuestion()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ViewResult CreateMultipleChoicesQuestion(MultipleChoicesQuestion question)
-        {
-            this.questions.Add(question);
-
-            return View();
-        }
-
-        [HttpGet]
-        public ViewResult CreateFreeTextQuestion()
-        {
-            return View();
-        }
-
-        [HttpPost]
-        public ViewResult CreateFreeTextQuestion(Question question)
-        {
-            this.questions.Add(question);
-
-            return View();
-        }
-
-        public ViewResult FinishQuestion()
-        {
-            this.ToString();
-
-            return null;
         }
     }
 }
---
+++ PollInTheAir/Controllers/PollController.cs
@@ -0,0 +1,51 @@
+﻿using System.Web.Mvc;
+using PollInTheAir.Domain.Models;
+
+namespace PollInTheAir.Web.Controllers
+{
+    public class PollController : Controller
+    {
+        [HttpGet]
+        public ViewResult CreatePoll()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CreatePoll(Poll poll)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult CreateMultipleChoicesQuestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CreateMultipleChoicesQuestion(MultipleChoicesQuestion question)
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult CreateFreeTextQuestion()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult CreateFreeTextQuestion(Question question)
+        {
+            return View();
+        }
+
+        public ViewResult FinishQuestion(MultipleChoicesQuestion question)
+        {
+            this.ToString();
+
+            return View("CreateMultipleChoicesQuestion");
+        }
+    }
+}
