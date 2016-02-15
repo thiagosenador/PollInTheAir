fe263e7018b07a05f8b78190bf0adc14297782f8
Thiago Senador <thiagosenadors@gmail.com>
2016-02-15 15:30:24 -0600

    spliting js scripts and applying some icons

---
+++ PollInTheAir/Scripts/intheair.multiplequestioncreation.js
@@ -0,0 +1,32 @@
+﻿$(function () {
+    $(document).on('click', '.btn-add', function (e) {
+        e.preventDefault();
+
+        var controlForm = $('.controls'),
+            currentEntry = $(this).parents('.entry:first'),
+            newEntry = $(currentEntry.clone()).appendTo(controlForm);
+
+        newEntry.find('input').val('');
+
+        $("[id=temp_name]").each(function (i) {
+            $(this).attr('name', 'Choices[' + i + '].Text');
+        });
+
+        controlForm.find('.entry:not(:last) .btn-add')
+            .removeClass('btn-add')
+            .addClass('btn-remove')
+            .removeClass('btn-primary')
+            .addClass('btn-warning')
+            .html('<span class="glyphicon glyphicon-minus"></span>');
+    }).on('click', '.btn-remove',
+        function (e) {
+            $(this).parents('.entry:first').remove();
+            e.preventDefault();
+
+            $("[id=temp_name]").each(function (i) {
+                $(this).attr('name', 'Choices[' + i + '].Text');
+            });
+
+            return false;
+        });
+});
