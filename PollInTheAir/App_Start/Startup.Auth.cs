b31b6dfe8f0f6ca36bcc973b7260303e57a737e6
thiagosenador <thiagosenadors@gmail.com>
2016-02-20 13:57:14 -0600

    design stuff

--- PollInTheAir/App_Start/Startup.Auth.cs
+++ PollInTheAir/App_Start/Startup.Auth.cs
@@ -1,8 +1,13 @@
 ﻿using System;
+using System.Net.Http;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin;
+using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.MicrosoftAccount;
+using Microsoft.Owin.Security.Twitter;
 using Owin;
 using PollInTheAir.Domain.Models;
 using PollInTheAir.Domain.Repository;
@@ -40,21 +45,42 @@ namespace PollInTheAir.Web
 
             app.UseGoogleAuthentication("1058035413863-pmbgn6n7slu0nph17l96nu65r9oeuqui.apps.googleusercontent.com", "J5wsfYCsgClB9G5miL7Bj8A7");
 
-            //app.UseMicrosoftAccountAuthentication(
-            //    clientId: "",
-            //    clientSecret: "");
+            var facebookOptions = new FacebookAuthenticationOptions()
+            {
+                AppId = "899746186808173",
+                AppSecret = "4a339373d1748469a27a021c376a6ac2",
+                BackchannelHttpHandler = new FacebookBackChannelHandler(),
+                UserInformationEndpoint = "https://graph.facebook.com/v2.4/me?fields=id,name,email,first_name,last_name,location"
+            };
 
-            //app.UseTwitterAuthentication(
-            //   consumerKey: "",
-            //   consumerSecret: "");
+            facebookOptions.Scope.Add("email");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "000000000000000",
-            //   appSecret: "000000000000000");
+            app.UseFacebookAuthentication(facebookOptions);
 
-            //app.UseGoogleAuthentication(
-            //     clientId: "000000000000000.apps.googleusercontent.com",
-            //     clientSecret: "000000000000000");
+            var microsoftOptions = new MicrosoftAccountAuthenticationOptions
+            {
+                Caption = "Live",
+                ClientId = "000000004018626B",
+                ClientSecret = "0JN-2FYhsi4z-NnskF3C97A63nSJbkW8"
+            };
+            microsoftOptions.Scope.Add("wl.basic");
+            microsoftOptions.Scope.Add("wl.emails");
+
+            app.UseMicrosoftAccountAuthentication(microsoftOptions);
+        }
+
+        public class FacebookBackChannelHandler : HttpClientHandler
+        {
+            protected override async System.Threading.Tasks.Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
+            {
+                // Replace the RequestUri so it's not malformed
+                if (!request.RequestUri.AbsolutePath.Contains("/oauth"))
+                {
+                    request.RequestUri = new Uri(request.RequestUri.AbsoluteUri.Replace("?access_token", "&access_token"));
+                }
+
+                return await base.SendAsync(request, cancellationToken);
+            }
         }
     }
 }
