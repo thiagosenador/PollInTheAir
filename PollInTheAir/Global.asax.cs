eaeb4f0c447a925b40909392ce74a80540d3082c
Thiago Senador <thiagosenadors@gmail.com>
2016-02-19 18:00:12 -0600

    new home page

--- PollInTheAir/Global.asax.cs
+++ PollInTheAir/Global.asax.cs
@@ -14,5 +14,13 @@ namespace PollInTheAir.Web
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
+
+        protected void Application_BeginRequest()
+        {
+            if (!Context.Request.IsSecureConnection)
+            {
+                Response.Redirect(Context.Request.Url.ToString().Replace("http:", "https:"));
+            }
+        }
     }
 }
--- PollInTheAir/ViewModel/AccountViewModel.cs
+++ PollInTheAir/ViewModel/AccountViewModel.cs
@@ -66,9 +66,6 @@ namespace PollInTheAir.Web.ViewModel
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
-
-        [Display(Name = "Hometown")]
-        public string Hometown { get; set; }
     }
 
     public class ResetPasswordViewModel
