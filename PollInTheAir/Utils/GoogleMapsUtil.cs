760daaca8a9b53b4f34dc58a334e7204cd543150
Thiago Senador <thiagosenadors@gmail.com>
2016-03-14 17:09:04 -0500

    map at end of publish

---
+++ PollInTheAir/Utils/GoogleMapsUtil.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PollInTheAir.Web.Utils
+{
+    public static class GoogleMapsUtil
+    {
+        public static void CreatePolyline(double latitude, double longitude, long radius)
+        {
+            var r = 6371;
+
+            var pi = Math.PI;
+
+            latitude = (latitude * pi) / 180;
+            longitude = (longitude * pi) / 180;
+
+            var d = radius / r;
+
+            var points = new List<double>();
+
+            for (int i = 0; i <= 360; i += 8)
+            {
+                var brng = i * pi / 180;
+                var pLat = Math.Asin((Math.Sin(latitude) * Math.Cos(d)) + (Math.Cos(latitude) * Math.Sin(d) * Math.Cos(brng)));
+                var pLng = ((longitude + Math.Atan2(Math.Sin(brng) * Math.Sin(d) * Math.Cos(latitude), Math.Cos(d) - (Math.Sin(latitude) * Math.Sin(pLat)))) * 180) / pi;
+                pLat = (pLat * 180) / pi;
+            }
+        }
+    }
+}
